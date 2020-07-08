5f4e7a36d9285402cbd32cefaa75f5d0e8097288
Hermino Junior <herminojunior@gmail.com>
2020-07-08 18:43:25 -0400

    Create pedidos_primeiro_semestre

---
+++ PostgreSQL/pedidos_primeiro_semestre.sql
@@ -0,0 +1,6 @@
+SELECT customers.name, orders.id
+FROM orders
+INNER JOIN customers
+ON orders.id_customers = customers.id
+WHERE orders.orders_date > '2016-01-01' 
+AND orders.orders_date < '2016-06-30';
